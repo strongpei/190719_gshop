c819dfbf52bafb1c4c9c03bce2712cf0c89ebb42
zhangpei <1154036267@qq.com>
2019-12-14 09:47:35 +0800

    11

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue"
+import 'lib-flexible'
+
+import App from "./App.vue"
+import router from "./router"
+import Header from './components/Header/Header.vue'
+
+Vue.component('Header',Header)
+
+
+new Vue({
+  render:h =>h(App),
+  router, //所有组件都能够看到$router和$route <router-link> <router-view/>>
+}).$mount('#app')
